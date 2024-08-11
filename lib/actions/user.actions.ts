7933af64b1357d50f7c6572f8d15dd6243559cd0
Spagestic <steelmax666@gmail.com>
2024-08-11 11:04:52 +0800

    Used clerkClient() as a function instead

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,12 +1,34 @@
-'use server';
+"use server";
 
 import { clerkClient } from "@clerk/nextjs/server";
 import { parseStringify } from "../utils";
 import { liveblocks } from "../liveblocks";
 
-export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
+// Clerk - DEPRECATION WARNING: "clerkClient singleton" is deprecated and will be removed in the next major release. Use clerkClient() as a function instead. 
+// export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
+//   try {
+//     const { data } = await clerkClient.users.getUserList({
+//       emailAddress: userIds,
+//     });
+
+//     const users = data.map((user) => ({
+//       id: user.id,
+//       name: `${user.firstName} ${user.lastName}`,
+//       email: user.emailAddresses[0].emailAddress,
+//       avatar: user.imageUrl,
+//     }));
+
+//     const sortedUsers = userIds.map((email) => users.find((user) => user.email === email));
+
+//     return parseStringify(sortedUsers);
+//   } catch (error) {
+//     console.log(`Error fetching users: ${error}`);
+//   }
+// }
+
+export const getClerkUsers = async ({ userIds }: { userIds: string[] }) => {
   try {
-    const { data } = await clerkClient.users.getUserList({
+    const { data } = await clerkClient().users.getUserList({
       emailAddress: userIds,
     });
 
@@ -17,24 +39,38 @@ export const getClerkUsers = async ({ userIds }: { userIds: string[]}) => {
       avatar: user.imageUrl,
     }));
 
-    const sortedUsers = userIds.map((email) => users.find((user) => user.email === email));
+    const sortedUsers = userIds.map((email) =>
+      users.find((user) => user.email === email)
+    );
 
     return parseStringify(sortedUsers);
   } catch (error) {
     console.log(`Error fetching users: ${error}`);
   }
-}
+};
 
-export const getDocumentUsers = async ({ roomId, currentUser, text }: { roomId: string, currentUser: string, text: string }) => {
+export const getDocumentUsers = async ({
+  roomId,
+  currentUser,
+  text,
+}: {
+  roomId: string;
+  currentUser: string;
+  text: string;
+}) => {
   try {
     const room = await liveblocks.getRoom(roomId);
 
-    const users = Object.keys(room.usersAccesses).filter((email) => email !== currentUser);
+    const users = Object.keys(room.usersAccesses).filter(
+      (email) => email !== currentUser
+    );
 
-    if(text.length) {
+    if (text.length) {
       const lowerCaseText = text.toLowerCase();
 
-      const filteredUsers = users.filter((email: string) => email.toLowerCase().includes(lowerCaseText))
+      const filteredUsers = users.filter((email: string) =>
+        email.toLowerCase().includes(lowerCaseText)
+      );
 
       return parseStringify(filteredUsers);
     }
@@ -43,4 +79,4 @@ export const getDocumentUsers = async ({ roomId, currentUser, text }: { roomId: 
   } catch (error) {
     console.log(`Error fetching document users: ${error}`);
   }
-}
+};
